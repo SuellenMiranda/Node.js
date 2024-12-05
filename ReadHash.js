7d7871f967e028a0dc0c4fa6ec22ae44870c390e
Suh <suellen.org@gmail.com>
2024-12-05 10:30:16 -0300

    Create ReadHash.js

---
+++ ReadHash.js
@@ -0,0 +1,16 @@
+// Read and Hash a File
+
+// crypto.mjs
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
+const hasher = createHash('sha1');
+
+hasher.setEncoding('hex');
+// ensure you have a `package.json` file for this test!
+hasher.write(await readFile('package.json'));
+hasher.end();
+
+const fileHash = hasher.read();
+
+// run with `node crypto.mjs`
